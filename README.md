a89fcde443fc26d084bfe8b0d2307778e5851cd0
ym-darake <sakamoto.takumi.dev@gmail.com>
2021-08-18 02:12:20 +0900

    Create README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# racket-guide
+Racket lang study based on
+ 1. https://docs.racket-lang.org/guide/macros.html
+ 2. https://www.greghendershott.com/fear-of-macros/all.html
--- continuation.rkt
+++ continuation.rkt
@@ -5,8 +5,22 @@
    (call/cc (lambda (k)
               (set! cc k)
               (+ 1 2))))
-; #<continuation>#
-
+; #<continuation>
 
 (+ 100 (cc 3))
 ; 9
+
+
+
+(define ccc '())
+
+(* 100
+   (* 3
+      (call/cc (lambda (k)
+                 (set! ccc k)
+                 (+ 1 2)))))
+; #<continuation>
+
+(+ 100 (cc 3))
+; 900
+
